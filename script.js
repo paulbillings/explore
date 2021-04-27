1440623213a26d50fcdecd205ed5843d996e653c
Paul Billings <paulbillings101@yahoo.co.uk>
2021-04-27 20:44:26 +0100

    added more smoke to foreground and finished off content section

--- script.js
+++ script.js
@@ -2,11 +2,12 @@ let controller = new ScrollMagic.Controller();
 let timeline = new TimelineMax();
 
 timeline
-  .to(".rock", 3, { y: -300 })
-  .to(".starship", 3, { y: -200 }, "-=3")
-  .fromTo(".bg1", { y: -50 }, { y: 0, duration: 3 }, "-=3")
-  .to(".content", 3, { top: "0%" }, "-=3")
-  .fromTo(".content-images", { opacity: 0.5 }, { opacity: 1, duration: 2 });
+  .to(".smoke3", 3, { y: -800 })
+  .to(".smoke", 3, { y: -600 }, "-=3")
+  .to(".starship", 3, { y: -300 }, "-=3")
+  .fromTo(".bg1", { y: -100 }, { y: 0, duration: 3 }, "-=3")
+  .to(".content", 3, { top: "0%" }, "-=3");
+// .fromTo(".content-images", { opacity: 0.5 }, { opacity: 1, duration: 2 });
 
 let scene = new ScrollMagic.Scene({
   triggerElement: "section",
